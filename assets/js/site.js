0ca713ad329018b9d20a07d4f961ced01a125a4d
aksigit <30791021+aksisoftsby@users.noreply.github.com>
2026-09-25 06:41:56 +0700

    Replicate original WP look (Customify + Elementor + MetaSlider) in Jekyll layouts

---
+++ assets/js/site.js
@@ -0,0 +1,44 @@
+/* Site glue JS: slider init, mobile nav, search toggle (mimics Customify + FlexSlider behavior). */
+(function ($) {
+  $(function () {
+    // FlexSlider init (MetaSlider equivalent)
+    if ($.fn.flexSlider) {
+      $('#metaslider_165.flexslider').flexslider({
+        animation: 'fade',
+        slideshowSpeed: 6000,
+        animationSpeed: 600,
+        controlNav: true,
+        directionNav: false,
+        pauseOnHover: true,
+        prevText: 'Previous',
+        nextText: 'Next'
+      });
+    }
+
+    // Mobile nav toggle
+    $(document).on('click', '.menu-mobile-toggle', function (e) {
+      e.preventDefault();
+      $('#mobile-nav-panel').addClass('open').attr('aria-hidden', 'false');
+    });
+    $(document).on('click', '.close-mobile-nav', function (e) {
+      e.preventDefault();
+      $('#mobile-nav-panel').removeClass('open').attr('aria-hidden', 'true');
+    });
+
+    // Header search toggle
+    $(document).on('click', '.header-search_icon-item .search-icon', function (e) {
+      e.preventDefault();
+      $(this).toggleClass('is-open');
+      $(this).siblings('.header-search-modal-wrapper').toggle();
+    });
+
+    // Product tabs
+    $(document).on('click', '.wc-tabs li a', function (e) {
+      e.preventDefault();
+      var $li = $(this).parent();
+      $li.addClass('active').siblings().removeClass('active');
+      $($('.wc-tabs-wrapper .panel')).hide();
+      $($(this).attr('href')).show();
+    });
+  });
+})(jQuery);
--- produk.md
+++ produk.md
@@ -1,22 +1,5 @@
 ---
-layout: page
-title: Produk Surendo
+layout: produk-all
+title: "Produk Surendo"
 permalink: /produk/
 ---
-
-<p>## Semua Produk</p>
-
-<p>### [Paket PJU Solarcell Standar]({{ '/product-category/pju-solarcell-stando/' | relative_url }})</p>
-<div class="cards">
-{% for p in site.products %}{% for c in p.categories %}{% if c.slug == 'pju-solarcell-stando' %}{% include product-card.html p=p %}{% endif %}{% endfor %}{% endfor %}
-</div>
-
-<p>### [PJU Solarcell All in One]({{ '/product-category/pju-solarcell-suritech-allinone/' | relative_url }})</p>
-<div class="cards">
-{% for p in site.products %}{% for c in p.categories %}{% if c.slug == 'pju-solarcell-suritech-allinone' %}{% include product-card.html p=p %}{% endif %}{% endfor %}{% endfor %}
-</div>
-
-<p>### [PJU Solarcell 2 In 1]({{ '/product-category/pju-solarcell-suritech-2-in-1/' | relative_url }})</p>
-<div class="cards">
-{% for p in site.products %}{% for c in p.categories %}{% if c.slug == 'pju-solarcell-suritech-2-in-1' %}{% include product-card.html p=p %}{% endif %}{% endfor %}{% endfor %}
-</div>
